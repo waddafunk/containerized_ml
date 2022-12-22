60e2eff732e6b64acb871e6d512289beec2a22dc
waddafunk <jacopo.piccirillo@gmail.com>
2022-12-22 22:46:41 +0100

    docker compose, redis

--- app/src/server.py
+++ app/src/server.py
@@ -1,6 +1,22 @@
 from flask import Flask
 import tensorflow as tf
+import redis
+import time
+
+print(__name__)
 server = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 @server.route("/")
 def hello():
@@ -10,5 +26,13 @@ def hello():
 def list_GPUs():
     return tf.config.list_physical_devices()
 
+@server.route("/cache_check")
+def get_hits():
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
+
 if __name__ == "__main__":
-   server.run(host='0.0.0.0')
+    print('Starting server...')
+    server.run(host='0.0.0.0')
+
+
